20af7193577dcb8cc3f4b325fa0a2e74e1b3364c
Gustavo So <so.gustavosantos@gmail.com>
2024-01-16 14:33:53 -0300

    Adicionando mais recursos à classe

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,13 +1,16 @@
 class restaurante:
-    nome = ''
-    categoria = ''
-    status_do_restaurante = False
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
 
-restaurante_serto = restaurante()
-restaurante_serto.nome = 'Sertó'
-restaurante_serto.categoria = 'Comida Mineira'
-restaurante_caracol = restaurante()
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
+
+restaurante_serto = restaurante('Sertó', 'Comida Mineira')
+restaurante_caracol = restaurante('Caracol Bar', 'Bar')
 
 restaurantes = [restaurante_serto, restaurante_caracol]
 
-print(vars(restaurante_serto))
+print(restaurante_serto)
+print(restaurante_caracol)
