e53e3d165738c8cda294be3fb02dbcede9388d50
Gustavo So <so.gustavosantos@gmail.com>
2024-02-07 11:16:26 -0300

    Alterando leitura do atributo "ativo"

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -2,17 +2,21 @@ class Restaurante:
     restaurantes = []
 
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
-        Restaurante.restaurantes.append(self)
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
+        Restaurante.restaurantes.append(self) #isso faz um append do próprio restaurante para a lista de restaurantes
 
     def __str__(self):
-        return f'{self.nome} | {self.categoria}'
+        return f'{self._nome} | {self._categoria}'
     
-    def listar_restaurantes():
+    def listar_restaurantes(): #função que substitui a parte de printar as informaçoes para cada um dos restaurantes no final do código
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante._ativo}')
+
+    @property
+    def ativo(self):
+        return 'Verdadeiro' if self._ativo else 'Falso'
 
 restaurante_serto = Restaurante('Sertó', 'Comida Mineira')
 restaurante_caracol = Restaurante('Caracol Bar', 'Bar')
