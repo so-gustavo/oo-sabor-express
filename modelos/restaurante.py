066959262438cccd224e63bab7d3f7fa19ee8a0d
Gustavo So <so.gustavosantos@gmail.com>
2024-02-07 11:46:28 -0300

    Adicionando função para alterar o status

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -10,15 +10,20 @@ class Restaurante:
     def __str__(self):
         return f'{self._nome} | {self._categoria}'
     
-    def listar_restaurantes(): #função que substitui a parte de printar as informaçoes para cada um dos restaurantes no final do código
-        for restaurante in Restaurante.restaurantes:
+    @classmethod
+    def listar_restaurantes(cls): #função que substitui a parte de printar as informaçoes para cada um dos restaurantes no final do código
+        for restaurante in cls.restaurantes:
             print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante._ativo}')
 
     @property
     def ativo(self):
         return 'Verdadeiro' if self._ativo else 'Falso'
+    
+    def alternar_estado(self):
+        self._ativo = not self._ativo
 
 restaurante_serto = Restaurante('Sertó', 'Comida Mineira')
+restaurante_serto.alternar_estado()
 restaurante_caracol = Restaurante('Caracol Bar', 'Bar')
 
 Restaurante.listar_restaurantes()
