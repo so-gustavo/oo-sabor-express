0a574647451f6fb4125ad8bddfc3f6c7a4fdd489
Gustavo So <so.gustavosantos@gmail.com>
2024-02-23 17:49:54 -0300

    Atualizando sistema de notas

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ restaurante_omotebako = Restaurante('Omotebako', 'Izakaya')
 
 restaurante_serto = Restaurante('Sertó', 'Comida Mineira')
 restaurante_serto.alternar_estado()
-restaurante_serto.receber_avaliacao('Sawyer', 10)
-restaurante_serto.receber_avaliacao('Joey', 8)
-restaurante_serto.receber_avaliacao('Isis', 6)
+restaurante_serto.receber_avaliacao('Sawyer', 5)
+restaurante_serto.receber_avaliacao('Joey', 4)
+restaurante_serto.receber_avaliacao('Isis', 4)
 
 restaurante_caracol = Restaurante('Caracol Bar', 'Bar')
 restaurante_omotebako.alternar_estado()
