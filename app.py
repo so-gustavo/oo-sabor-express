84c2c21b9a5497486c92da2226f7071e70350f5c
Gustavo So <so.gustavosantos@gmail.com>
2024-02-26 15:40:10 -0300

    Refatorando métodos de adicionar itens ao cardápio

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from modelos.cardapio.prato import Prato
 restaurante_omotebako = Restaurante('Omotebako', 'Izakaya')
 bebida_suco = Bebida('Negroni', 22.0, 'único')
 prato_baiao_de_dois = Prato('Baião de Dois', 59.0, 'Com feijão fradinho, carne seca, queijo coalho e especiarias nordestinas')
-restaurante_omotebako.adicionar_bebida_no_cardapio(bebida_suco)
-restaurante_omotebako.adicionar_prato_no_cardapio(prato_baiao_de_dois)
+restaurante_omotebako.adicionar_no_cardapio(bebida_suco)
+restaurante_omotebako.adicionar_no_cardapio(prato_baiao_de_dois)
 
 def main():
     print(bebida_suco)
--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,4 +1,5 @@
 from modelos.avaliacoes import Avaliacao
+from modelos.cardapio.item_cardapio import ItemCardapio
 
 class Restaurante:
     restaurantes = []
@@ -45,3 +46,7 @@ class Restaurante:
 
     def adicionar_prato_no_cardapio(self, prato):
         self._cardapio.append(prato)
+
+    def adicionar_no_cardapio(self, item):
+        if isinstance(item, ItemCardapio):
+            self._cardapio.append(item)
