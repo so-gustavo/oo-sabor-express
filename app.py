385db677e0882ad8772fad661cf7c38959d2a098
Gustavo So <so.gustavosantos@gmail.com>
2024-02-28 22:18:00 -0300

    Criando método para exibição do cardápio

--- app.py
+++ app.py
@@ -3,14 +3,13 @@ from modelos.cardapio.bebida import Bebida
 from modelos.cardapio.prato import Prato
 
 restaurante_omotebako = Restaurante('Omotebako', 'Izakaya')
-bebida_suco = Bebida('Negroni', 22.0, 'único')
+bebida_suco = Bebida('Negroni', 22.0, 'Grande')
 prato_baiao_de_dois = Prato('Baião de Dois', 59.0, 'Com feijão fradinho, carne seca, queijo coalho e especiarias nordestinas')
 restaurante_omotebako.adicionar_no_cardapio(bebida_suco)
 restaurante_omotebako.adicionar_no_cardapio(prato_baiao_de_dois)
 
 def main():
-    print(bebida_suco)
-    print(prato_baiao_de_dois)
+    restaurante_omotebako.exibir_cardapio
 
 if __name__ == '__main__':
     main()
--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -40,13 +40,18 @@ class Restaurante:
         quantidade_de_notas = len(self._avaliacao)
         media = round(soma_das_notas / quantidade_de_notas, 1)
         return media
-    
-    def adicionar_bebida_no_cardapio(self, bebida):
-        self._cardapio.append(bebida)
-
-    def adicionar_prato_no_cardapio(self, prato):
-        self._cardapio.append(prato)
 
     def adicionar_no_cardapio(self, item):
         if isinstance(item, ItemCardapio):
             self._cardapio.append(item)
+
+    @property
+    def exibir_cardapio(self):
+        print(f'Cardapio do Restaurante {self._nome}\n')
+        for i, item in enumerate(self._cardapio, start = 1):
+            if hasattr(item, 'descricao'):
+                mensagem_prato = f'{i}. Nome: {item._nome} | Preço: R${item._preco} | Descrição: {item._descricao}'
+                print(mensagem_prato)
+            else:
+                mensagem_bebida = f'{i}. Nome: {item._nome} | Preço: R${item._preco} | Tamanho: {item._tamanho}'
+                print(mensagem_bebida)
